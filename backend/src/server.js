31f181de8218c8893bb75d2125a8b0ee2b255337
vijayharshamade <vharsha736.vh@gmail.com>
2025-12-12 18:18:16 +0530

    Integrated Rate Limiting with Upstash

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,28 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config(); // without this line you cannot access .env
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
+//middleware
 app.use(express.json());
+app.use(rateLimiter);
+
+// simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(
+//     `the requested method is ${req.method} and requested url is ${req.url}`
+//   );
+//   next();
+// });
 app.use("/api/notes", notesRoutes);
-app.listen(PORT, () => {
-  console.log("Server started on PORT:", PORT);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
